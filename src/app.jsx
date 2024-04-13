e78b132a3b906a0032737d9760e82801baf49b87
=DINEZZZZZ <=dineshasus2023@gmail.com>
2024-04-13 16:22:07 +0530

    first dep

--- src/app.jsx
+++ src/app.jsx
@@ -16,7 +16,7 @@ export function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/mensural" element={<LandingPage />} />
         <Route path="/home" element={<Home/>}/>
         <Route path="/signup" element={<SignupForm />} />
         <Route path="/signin" element={<SigninForm />} />
